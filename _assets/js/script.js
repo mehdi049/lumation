9423f0688629bc8babdd407534ac51cc0a08a533
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-07-16 02:02:48 +0100

    - add form

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,8 +1,83 @@
 $(function () {
-  $("#open-menu-mobile").click(function () {
-    $("#menu-mobile").animate({ "margin-right": "0px" }, 500);
+  $("#step-1-submit").click(function () {
+    $("#form-step1").addClass("d-none");
+    $("#form-step2").removeClass("d-none");
   });
-  $("#close-menu-mobile").click(function () {
-    $("#menu-mobile").animate({ "margin-right": "-250px" }, 500);
+
+  $("#step-2-submit").click(function () {
+    $("#form-step1").addClass("d-none");
+    $("#form-step2").addClass("d-none");
+    $("#form-step3").removeClass("d-none");
+  });
+
+  $("#step-3-submit").click(function () {
+    setTimeout(() => {
+      $("#form-step1").removeClass("d-none");
+      $("#form-step3").addClass("d-none");
+    }, 1500);
+  });
+
+  $("input[name=cotizar-option]").change(function () {
+    let value = $(this).val();
+    switch (value) {
+      case "marítimo":
+        $(
+          "#form-step2-2, #form-step2-3, #form-step2-4, #form-step2-5"
+        ).addClass("d-none");
+        $("#form-step2-1").removeClass("d-none");
+        break;
+      case "aéreo":
+        $(
+          "#form-step2-1, #form-step2-3, #form-step2-4, #form-step2-5"
+        ).addClass("d-none");
+        $("#form-step2-2").removeClass("d-none");
+        break;
+      case "terrestre":
+        $(
+          "#form-step2-1, #form-step2-2, #form-step2-4, #form-step2-5"
+        ).addClass("d-none");
+        $("#form-step2-3").removeClass("d-none");
+        break;
+      case "aduana":
+        $(
+          "#form-step2-1, #form-step2-2, #form-step2-3, #form-step2-5"
+        ).addClass("d-none");
+        $("#form-step2-4").removeClass("d-none");
+        break;
+      case "seguro":
+        $(
+          "#form-step2-1, #form-step2-2, #form-step2-3, #form-step2-4"
+        ).addClass("d-none");
+        $("#form-step2-5").removeClass("d-none");
+        break;
+    }
+  });
+
+  $("input[name=maritimo-option]").change(function () {
+    let value = $(this).val();
+    switch (value) {
+      case "LCL":
+        $("#form-step2-1-2").addClass("d-none");
+        $("#form-step2-1-1").removeClass("d-none");
+        break;
+      case "FCL":
+        $("#form-step2-1-1").addClass("d-none");
+        $("#form-step2-1-2").removeClass("d-none");
+        break;
+    }
+  });
+
+  $("input[name=terrestre-option]").change(function () {
+    let value = $(this).val();
+    switch (value) {
+      case "LTL":
+        $("#form-step2-3-2").addClass("d-none");
+        $("#form-step2-3-1").removeClass("d-none");
+        break;
+      case "FTL":
+        $("#form-step2-3-1").addClass("d-none");
+        $("#form-step2-3-2").removeClass("d-none");
+        break;
+    }
   });
 });
